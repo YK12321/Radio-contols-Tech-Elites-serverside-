2806b3e84c6209b2c81d38ba66844dd0972297cd
YK12321 <99835099+YK12321@users.noreply.github.com>
2022-12-13 16:36:01 -0800

    Create radio.cpp

---
+++ program here/radio.cpp
@@ -0,0 +1,22 @@
+#include "radio.h"
+
+void on()
+{
+    // implementation details for turning on the radio module
+}
+
+void config(int frequency, int power)
+{
+    // implementation details for configuring the radio module
+}
+
+void send_string(std::string message)
+{
+    // implementation details for transmitting a string over radio
+}
+
+std::string receive_string()
+{
+    // implementation details for receiving a string over radio
+    return "";
+}
