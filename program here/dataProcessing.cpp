80c9c9c9a718e01cd9bad904ce4f2ef5453158e5
Justin Jiang <justinjiang37@outlook.com>
2023-01-19 21:48:10 -0800

    WIP Graphing

---
+++ program here/dataProcessing.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+double stoi(string data);
+
+int main() {
+    // Temperature - Celsius
+    double temp_ideal = 28.0;
+    double temp_dangerously_high = 57.0;
+    double temp_dangerously_low = -70;
+
+    // Pressure - kPa
+    double pres_ideal = 100.0;
+    double pres_dangerously_high = 10132.0;
+    double pres_dangerously_low = 6.0;
+
+    // Humidity - percent
+    double humidity_ideal = 40.0;
+
+    // Radiation - UV-index
+    double uv_ideal = 4.0;
+    double uv_dangerously_high = 11.0;
+
+    // Others
+    double acc_constant;
+
+    // Graphing WIP
+    // https://www.youtube.com/watch?v=sGximvTFJpc
+
+    return 0;
+}
